a585b4cc2a8f62752aaa409eb5e1b0f5a079f389
husnahussaini <husna.hussaini89@gmail.com>
2026-07-13 20:46:22 +0430

    Add Course class

---
+++ course.py
@@ -0,0 +1,32 @@
+class Course:
+    def __init__(self, course_code, course_name):
+        self.course_code = course_code
+        self.course_name = course_name
+        self.students = []
+        self.assessments = []
+
+    def add_student(self, student_id):
+        if student_id not in self.students:
+            self.students.append(student_id)
+        else:
+            print("Student is already enrolled in this course.")
+
+    def add_assessment(self, assessment):
+        self.assessments.append(assessment)
+
+    def find_assessment(self,title):
+        for assessment in self.assessments:
+            if assessment.title.lower() == title.lower():
+                return assessment
+        return None
+
+    def display_info(self):
+        print(f"Course Code: {self.course_code}")
+        print(f"Course Name: {self.course_name}")
+        print(f"Enrolled Students: {len(self.students)}")
+        print("Assessments:")
+        if self.assessments:
+            for assessment in self.assessments:
+                assessment.display_info()
+        else:
+            print("No assessments added.")
