977fc4e5064219feb479ed2c9384cd81c786b482
husnahussaini <husna.hussaini89@gmail.com>
2026-07-16 00:05:28 +0430

    Add Assessment grade_message

--- assessment.py
+++ assessment.py
@@ -5,3 +5,10 @@ class Assessment:
 
     def calculate_percentage(self, score):
         return (score / self.max_score) * 100
+
+    def grade_message(self, score):
+        percentage = self.calculate_percentage(score)
+        if percentage >= 55:
+            return "Good job!"
+        else:
+            return "Needs improvement."
