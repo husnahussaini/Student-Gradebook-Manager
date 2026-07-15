9c651e22219ea4427ea2001cb8979e5f4e394831
husnahussaini <husna.hussaini89@gmail.com>
2026-07-15 23:57:59 +0430

    Add percentage calculation

--- assessment.py
+++ assessment.py
@@ -2,3 +2,6 @@ class Assessment:
     def __init__(self, title, max_score):
         self.title = title
         self.max_score = max_score
+
+    def calculate_percentage(self, score):
+        return (score / self.max_score) * 100
