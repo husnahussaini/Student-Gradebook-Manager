fa00f8941f32dd85c5395e8641c05acda282d2f2
husnahussaini <husna.hussaini89@gmail.com>
2026-07-16 00:18:47 +0430

    Override Quiz grade_message

--- assessment.py
+++ assessment.py
@@ -19,3 +19,10 @@ class Assessment:
 class Quiz(Assessment):
     def display_info(self):
         print(f"Quiz: {self.title} / max_score: {self.max_score}")
+
+    def grade_message(self, score):
+        percentage = self.calculate_percentage(score)
+        if percentage >= 80
+            return "Great quiz result!"
+        else:
+            return "Needs more practice."
