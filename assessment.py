b00efd32f6c7e1eb18a01aafcdbef0d89183e4aa
husnahussaini <husna.hussaini89@gmail.com>
2026-07-16 00:32:35 +0430

    Add Exam class

--- assessment.py
+++ assessment.py
@@ -26,3 +26,15 @@ class Quiz(Assessment):
             return "Great quiz result!"
         else:
             return "Needs more practice."
+
+class Exam(Assessment):
+    def display_info(self):
+        print(f"Exam: {self.title} / Max Score: {self.max_score}")
+
+    def grade_message(self, score):
+        percentage = self.calculate_percentage(score)
+        if percentage >= 55:
+            return "Passed exam."
+        else:
+            return "Failed exam."
+
